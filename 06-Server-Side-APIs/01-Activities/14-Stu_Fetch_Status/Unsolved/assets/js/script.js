2536b5b5cae30f91ea131dad6b655cd5bdab8468
Payton Whinnery <paytoncaliw@gmail.com>
2021-01-27 18:32:30 -0600

    pushing changes

--- 06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/14-Stu_Fetch_Status/Unsolved/assets/js/script.js
@@ -2,13 +2,16 @@ var badRequestUrl = 'https://api.github.com/orgs/nodejs/oreps?per_page=5';
 
 var responseText = document.getElementById('response-text');
 
-function getApi(request) {
+function getApi(requestUrl) {
   fetch(requestUrl)
     .then(function (response) {
       // Check the console first to see the response.status
       console.log(response.status);
       // Then write the conditional based on that response.status value
       // Make sure to display the response on the page
+      if (response.status === 404) {
+        responseText.textContent = response.status;
+      }
     })
     .then(function (data) {
       console.log(data);
--- 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/20-Stu_Document_Location/Unsolved/assets/js/script.js
@@ -2,6 +2,10 @@ var badRequestUrl = 'https://api.github.com/unicorns';
 var redirectUrl = './404.html';
 
 fetch(badRequestUrl).then(function (response) {
+  if (response.status === 404) {
+    window.location.replace(redirectUrl);
+
+  }
   // Use a conditional to check the response status.
   // If that status equals the conditional, then redirect to the 404 page.
 });
